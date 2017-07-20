be2aa0f7cd6308fe6b917bd965b5773896ffd398
Ant and Buffalo <antandbuffalo@gmail.com>
2017-07-20 19:44:08 +0800

    restore file updated

--- app/src/main/java/com/antandbuffalo/birthdayreminder/restore/RestoreBackup.java
+++ app/src/main/java/com/antandbuffalo/birthdayreminder/restore/RestoreBackup.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.antandbuffalo.birthdayreminder.Constants;
+import com.antandbuffalo.birthdayreminder.DataHolder;
 import com.antandbuffalo.birthdayreminder.DateOfBirth;
 import com.antandbuffalo.birthdayreminder.R;
 import com.antandbuffalo.birthdayreminder.Util;
@@ -28,10 +30,35 @@ public class RestoreBackup extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restore_backup);
 
-        ImageButton save = (ImageButton) findViewById(R.id.save);
-        save.setBackgroundResource(R.drawable.restore_ok_button);
+        ImageButton restoreOk = (ImageButton) findViewById(R.id.save);
+        restoreOk.setBackgroundResource(R.drawable.save_button);
 
-        ImageButton cancel = (ImageButton)findViewById(R.id.cancel);
-        cancel.setBackgroundResource(R.drawable.cancel_button);
+        ImageButton restoreCancel = (ImageButton)findViewById(R.id.cancel);
+        restoreCancel.setBackgroundResource(R.drawable.cancel_button);
+
+        restoreOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Util.readFromFile(Constants.FILE_NAME);
+                SharedPreferences settings = getSharedPreferences(Constants.PREFERENCE_NAME, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                for (int i = 0; i < DataHolder.getInstance().refreshTracker.size(); i++) {
+                    DataHolder.getInstance().refreshTracker.set(i, true);
+                }
+                Toast toast = Toast.makeText(getApplicationContext(), Constants.NOTIFICATION_SUCCESS_DATA_LOAD, Toast.LENGTH_SHORT);
+                toast.show();
+                editor.putBoolean("isSecondTime", true);
+                editor.commit();
+                finish();
+            }
+        });
+
+        restoreCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
