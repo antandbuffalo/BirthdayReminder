6aea60f190f86893616ce5bc6326cadfb4ca57f5
Ant and Buffalo <antandbuffalo@gmail.com>
2017-08-16 12:28:21 +0800

    about us page added

--- app/src/main/java/com/antandbuffalo/birthdayreminder/about/About.java
+++ app/src/main/java/com/antandbuffalo/birthdayreminder/about/About.java
@@ -1,7 +1,10 @@
 package com.antandbuffalo.birthdayreminder.about;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.antandbuffalo.birthdayreminder.R;
 
@@ -11,6 +14,21 @@ public class About extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+
+        TextView contactUs = (TextView)findViewById(R.id.contactUs);
+        contactUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                emailIntent.setType("vnd.android.cursor.item/email");
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] {"antandbuffalo@gmail.com"});
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Feedback of Birthday Reminder app");
+                //emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+                startActivity(Intent.createChooser(emailIntent, "Send mail using..."));
+                //startActivity(emailIntent);
+            }
+        });
     }
 
 }
