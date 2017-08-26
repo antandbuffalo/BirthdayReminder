a77dd279a71f630fac279d59a6e619f0c24036aa
Jeyabalaji <balaji.jeya@gmail.com>
2017-08-26 15:43:20 +0800

    First production release

--- app/src/main/java/com/antandbuffalo/birthdayreminder/about/About.java
+++ app/src/main/java/com/antandbuffalo/birthdayreminder/about/About.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.antandbuffalo.birthdayreminder.BuildConfig;
@@ -20,17 +21,17 @@ public class About extends Activity {
         TextView version = (TextView)findViewById(R.id.version);
         version.setText("Version " + BuildConfig.VERSION_NAME + " (" + BuildConfig.VERSION_CODE + ")");
 
-        TextView contactUs = (TextView)findViewById(R.id.contactUs);
+        Button contactUs = (Button)findViewById(R.id.contactUs);
         contactUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
                 emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 emailIntent.setType("vnd.android.cursor.item/email");
-                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{Constants.DEVELOPER_EMAIL});
-                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Feedback of Birthday Reminder app");
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{Constants.FEEDBACK_EMAIL});
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, Constants.FEEDBACK_EMAIL_SUBJECT);
                 //emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
-                startActivity(Intent.createChooser(emailIntent, "Send mail using..."));
+                startActivity(Intent.createChooser(emailIntent, Constants.FEEDBACK_EMAIL_POPUP_MESSAGE));
                 //startActivity(emailIntent);
             }
         });
