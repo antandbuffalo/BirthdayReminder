6c051c60d3cb4e7519c10be4633c459bf3a731a5
Jeyabalaji <balaji.jeya@gmail.com>
2018-07-31 23:11:25 +0800

    feature: wish template activity added

---
+++ app/src/main/java/com/antandbuffalo/birthdayreminder/wishtemplate/WishTemplate.kt
@@ -0,0 +1,14 @@
+package com.antandbuffalo.birthdayreminder.wishtemplate
+
+import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.antandbuffalo.birthdayreminder.R
+
+class WishTemplate : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.wish_template)
+    }
+}
